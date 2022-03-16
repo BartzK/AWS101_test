36a4b9ee50d87e6bf56cf6533fcb0af0d7eb49bd
BartzK <bartoszkreft@gmail.com>
2022-03-16 10:24:12 +0100

    Initial commit

---
+++ AWS101/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AWS101.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from REST API";
+        }
+    }
+}
