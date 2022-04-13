d0d5d462ee4f27a308725208d393dd65f2c06e30
BartzK <bartoszkreft@gmail.com>
2022-04-13 10:01:56 +0200

    terrraform final base

--- terraform-aws-ecr/aws_login.bat
+++
@@ -1,5 +0,0 @@
-set region=us-east-1
-set aws_account_id=281738164247
-
-aws ecr get-login-password --region %region% | docker login --username AWS --password-stdin %aws_account_id%.dkr.ecr.%region%.amazonaws.com
-
--- terraform-aws-ecs/ecs.tf
+++ terraform-aws-ecs/ecs.tf
@@ -8,13 +8,17 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
   region  = "eu-central-1"
 }
 
 variable "vpc_id" {
   type    = string
   default = "vpc-0fead40e24304ce5f"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-0c5ab4a1499db9f85"
 }
 
 variable "user_prefix" {
@@ -24,6 +28,10 @@ variable "user_prefix" {
 
 data "aws_vpc" "vpc" {
   id = var.vpc_id
+}
+
+data "aws_subnet" "snet" {
+  id = var.subnet_id
 }
 
 resource "aws_ecr_repository" "ecr" {
@@ -56,12 +64,35 @@ resource "aws_ecr_repository_policy" "repo-policy" {
   EOF
 }
 
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "${var.user_prefix}-execution-task-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+resource "aws_security_group" "bk-security-group" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
+resource "aws_iam_role" "ecs-instance-role" {
+  name               = "${var.user_prefix}-ecs-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecs-instance-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs-policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -72,30 +103,30 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
+resource "aws_iam_role" "ec2-instance-role" {
+  name               = "${var.user_prefix}-ec2-instance-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ec2TaskExecutionRole_policy" {
+  role       = aws_iam_role.ec2-instance-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+data "aws_iam_policy_document" "ec2-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "bk-instance-profile"
-  role = aws_iam_role.ecsTaskExecutionRole.name
-}
-
-resource "aws_instance" "app_server" {
-    ami                          = "ami-0143f2b57717ab830"
-    associate_public_ip_address  = true
-    instance_type                = "t2.micro"
-	iam_instance_profile         = aws_iam_instance_profile.instance_profile.name
-    tags                         = {
-        "Name" = "bk-instance"
-    }
-	
-	user_data = <<EOF
-	#!/bin/bash
-	echo ECS_CLUSTER=${aws_ecs_cluster.ecs-cluster.name} >> /ets/ecs/ecs.config
-	EOF
-
+  role = aws_iam_role.ec2-instance-role.name
 }
 
 resource "aws_ecs_cluster" "ecs-cluster" {
@@ -106,22 +137,44 @@ resource "aws_ecs_cluster" "ecs-cluster" {
   }
 }
 
+resource "aws_launch_configuration" "ecs-launch-configuration" {
+  name                 = "${var.user_prefix}-launch-configuration"
+  image_id             = "ami-0c114f68881dc4f44"
+  instance_type        = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.instance_profile.name}"
+  key_name = "bk-key-pair"
+  security_groups = [aws_security_group.bk-security-group.id]
+  associate_public_ip_address = "true"
+  user_data                   = <<DEFINITION
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.ecs-cluster.name} >> /etc/ecs/ecs.config;
+DEFINITION
+}
+
+resource "aws_autoscaling_group" "autoscaling_group" {
+  name                      = "${var.user_prefix}-autoscaling"
+  vpc_zone_identifier  = [data.aws_subnet.snet.id]
+  launch_configuration      = aws_launch_configuration.ecs-launch-configuration.name
+  min_size                  = 1
+  max_size                  = 10
+  desired_capacity          = 1
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+}
+
 resource "aws_ecs_task_definition" "ecs-task-definition" {
   family                   = "${var.user_prefix}-definition"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["EC2"]
-  memory                   = "256"
-  cpu                      = "256"
-  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
-  task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
+  memory                   = "512"
+  cpu                      = "1vcpu"
+  execution_role_arn       = aws_iam_role.ecs-instance-role.arn
+  task_role_arn            = aws_iam_role.ecs-instance-role.arn
   container_definitions    = <<DEFINITION
   [
     {
       "name": "${var.user_prefix}-container",
       "image": "${aws_ecr_repository.ecr.repository_url}/${var.user_prefix}-repository:latest",
-      "memory": 256,
-      "cpu": 256,
-      "networkMode": "awsvpc",
+      "memory": 512,
+      "cpu": 1,
       "essential": true,
       "portMappings": [
         {
@@ -144,9 +197,5 @@ resource "aws_ecs_service" "ecs-service" {
   task_definition = aws_ecs_task_definition.ecs-task-definition.arn
   launch_type     = "EC2"
   desired_count   = 1
-  
-  network_configuration {
-    subnets        = ["subnet-020c346673e0afe4f"]
-    security_groups = ["sg-0f7ef34ae71da89ef"]
-  }
+  enable_ecs_managed_tags = true
 }
